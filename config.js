c4a7fbecbf95a707039eb23b443b1cdd58089849
Teja <145374075+teja-0311@users.noreply.github.com>
2025-07-23 23:17:35 +0530

    Add frontend config.js for Expo

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-import { config } from 'dotenv';
-config();
+import Constants from 'expo-constants';
 
-export const API_BASE_URL = 'http://192.168.2.9:5000';
-export const CLOUDINARY_UPLOAD_URL = process.env.CLOUDINARY_UPLOAD_URL;
-
+export const API_BASE_URL = Constants.expoConfig.extra.apiUrl;
+export const CLOUDINARY_UPLOAD_URL = Constants.expoConfig.extra.cloudinaryUrl;
